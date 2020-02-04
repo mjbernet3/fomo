4d5c0e3b6dd2d061f84e0690b0dad9d15fa7fa38
Emma Chadwick <emmachadwick@lawn-143-215-122-129.lawn.gatech.edu>
2020-02-04 00:11:21 -0500

    InputField style fixes

--- lib/frontend/components/inputFieldWidget.dart
+++ lib/frontend/components/inputFieldWidget.dart
@@ -1,28 +1,28 @@
 import 'package:flutter/material.dart';
 
-Widget inputField(String title, IconData i, Function changed, [bool hidden = false]) {
-  return TextField(
-      autofocus: true,
-      obscureText: hidden,
-      focusNode: FocusNode(),
-      decoration: InputDecoration(
-          focusedBorder: UnderlineInputBorder(
-              borderSide: const BorderSide(color: Colors.white)
-          ),
-          border: UnderlineInputBorder(),
-          labelText: title,
-          suffixIcon: Padding(
-              padding: const EdgeInsetsDirectional.only(end: 12),
-              child: Icon(i, color: Colors.white)
-          ),
-          labelStyle: TextStyle(
-              fontSize: 20,
-              fontFamily: 'Raleway',
-              color: Colors.white
-          )
-      ),
-      onChanged: (String typedValue) {
-        changed(typedValue);
-      }
-  );
+Widget inputField(String title, IconData i, Function changed,
+    [bool hidden = false]) {
+  return Column(children: <Widget>[
+    Align(
+        alignment: Alignment.topLeft,
+        child: Text(title,
+            style: TextStyle(
+                fontSize: 20, fontFamily: 'Raleway', color: Colors.white))),
+    TextField(
+        autofocus: true,
+        obscureText: hidden,
+        focusNode: FocusNode(),
+        decoration: InputDecoration(
+            focusedBorder: UnderlineInputBorder(
+                borderSide: const BorderSide(color: Colors.white)),
+            border: UnderlineInputBorder(),
+            suffixIcon: Padding(
+                padding: const EdgeInsetsDirectional.only(end: 12),
+                child: Icon(i, color: Colors.white)),
+            labelStyle: TextStyle(
+                fontSize: 20, fontFamily: 'Raleway', color: Colors.white)),
+        onChanged: (String typedValue) {
+          changed(typedValue);
+        })
+  ]);
 }

c9fd55a4e32479ac13ee5c877376afc3d0480b22
Matthew Bernet <mattbernetGT@gmail.com>
2020-02-24 23:54:29 -0500

    Event scroller refactoring and added transition to view all

--- lib/frontend/components/discover/event_scroller.dart
+++ lib/frontend/components/discover/event_scroller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:project_fomo/backend/models/event.dart';
 import 'package:project_fomo/frontend/components/discover/event_card.dart';
+import 'package:project_fomo/frontend/pages/view_all_page.dart';
 import 'package:project_fomo/style.dart';
 
 class EventScroller extends StatelessWidget {
@@ -13,67 +14,72 @@ class EventScroller extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        srollerHeader(title),
-        scroller(events),
+        Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 10.0,
+            horizontal: 10.0,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.baseline,
+            textBaseline: TextBaseline.alphabetic,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(3.0),
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    color: AppTextColor.title,
+                    fontSize: AppFontSize.size18,
+                    fontFamily: AppFontFamily.family,
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, ViewAllPage.pageRoute);
+                },
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      "View All",
+                      style: TextStyle(
+                        color: AppTextColor.mediumEmphasis,
+                        fontSize: AppFontSize.size14,
+                        fontFamily: AppFontFamily.family,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 6.0),
+                      child: Icon(
+                        Icons.arrow_forward_ios,
+                        size: AppFontSize.size14,
+                        color: AppTextColor.mediumEmphasis,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 220,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            itemCount: events.length,
+            itemBuilder: (context, index) => SizedBox(
+              width: 15.0,
+            ),
+            separatorBuilder: (context, index) => SizedBox(
+              width: 220,
+              child: SmallEventCard.fromEvent(
+                events[index],
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
-}
-
-Widget srollerHeader(String title) {
-  return Padding(
-    padding: EdgeInsets.all(10.0),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.baseline,
-      textBaseline: TextBaseline.alphabetic,
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.all(3.0),
-          child: Text(
-            title,
-            style: TextStyle(),
-          ),
-        ),
-        viewAllButton(),
-      ],
-    ),
-  );
-}
-
-Widget viewAllButton() {
-  return GestureDetector(
-    onTap: null,
-    child: Row(
-      children: <Widget>[
-        Text(
-          "View All",
-          style: TextStyle(),
-        ),
-        Padding(
-          padding: EdgeInsets.only(left: 10.0),
-          child: Icon(
-            Icons.arrow_forward_ios,
-            size: AppFontSize.size16,
-            color: AppColor.dp12,
-          ),
-        ),
-      ],
-    ),
-  );
-}
-
-Widget scroller(List<Event> events) {
-  return SizedBox(
-    height: 250,
-    child: ListView.separated(
-      scrollDirection: Axis.horizontal,
-      itemCount: events.length,
-      itemBuilder: (context, index) => SizedBox(
-        width: 15.0,
-      ),
-      separatorBuilder: (context, index) =>
-          SizedBox(width: 275, child: SmallEventCard.fromEvent(events[index])),
-    ),
-  );
 }

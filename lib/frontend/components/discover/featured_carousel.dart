ecac80b2dc14a5ec0f938b04fcbee549e49ddb16
Matthew Bernet <mattbernetGT@gmail.com>
2020-02-26 22:23:37 -0500

    Remove current state management approach

--- lib/frontend/components/discover/featured_carousel.dart
+++ lib/frontend/components/discover/featured_carousel.dart
@@ -1,11 +1,10 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:project_fomo/backend/models/event.dart';
-import 'package:project_fomo/backend/state_models/carousel_model.dart';
 import 'package:project_fomo/frontend/components/discover/carousel_ellipse.dart';
 import 'package:project_fomo/frontend/components/discover/event_card.dart';
-import 'package:project_fomo/frontend/components/state_widget.dart';
 import 'package:project_fomo/style.dart';
+import 'package:provider/provider.dart';
 
 class FeaturedCarousel extends StatelessWidget {
   final List<Event> events;
@@ -33,43 +32,56 @@ class FeaturedCarousel extends StatelessWidget {
             ),
           ),
         ),
-        StateWidget<CarouselModel>(
-          stateModel: CarouselModel(),
-          builder: (context, model, child) {
-            List<Widget> ellipses = [];
-            for (var i = 0; i < events.length; i++) {
-              ellipses.add(CarouselEllipse(activated: (i == model.index)));
-            }
-            return Column(
-              children: <Widget>[
-                CarouselSlider(
-                  height: 220,
-                  viewportFraction: 1.1,
-                  onPageChanged: (int index) => (model.index = index),
-                  items: events
-                      .map<Widget>(
-                        (event) => Padding(
-                          padding: EdgeInsets.only(left: 30.0, right: 30.0),
-                          child: SmallEventCard(event: event),
-                        ),
-                      )
-                      .toList(),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-                  child: SizedBox(
-                    width: 100,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: ellipses,
+        ChangeNotifierProvider<CarouselModel>(
+          create: (context) => CarouselModel(),
+          child: Consumer<CarouselModel>(
+            builder: (context, model, child) {
+              List<Widget> ellipses = [];
+              for (var i = 0; i < events.length; i++) {
+                ellipses.add(CarouselEllipse(activated: (i == model.index)));
+              }
+              return Column(
+                children: <Widget>[
+                  CarouselSlider(
+                    height: 220,
+                    viewportFraction: 1.1,
+                    onPageChanged: (int index) => (model.index = index),
+                    items: events
+                        .map<Widget>(
+                          (event) => Padding(
+                            padding: EdgeInsets.only(left: 30.0, right: 30.0),
+                            child: SmallEventCard(event: event),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                    child: SizedBox(
+                      width: 100,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: ellipses,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            );
-          },
+                ],
+              );
+            },
+          ),
         ),
       ],
     );
+  }
+}
+
+class CarouselModel extends ChangeNotifier {
+  int _index = 0;
+
+  int get index => _index;
+
+  set index(int index) {
+    this._index = index;
+    notifyListeners();
   }
 }
--- lib/frontend/pages/register_page.dart
+++ lib/frontend/pages/register_page.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:project_fomo/frontend/components/shared/page_header.dart';
 import 'package:project_fomo/frontend/components/shared/loading_indicator.dart';
-import 'package:project_fomo/frontend/components/state_widget.dart';
 import 'package:project_fomo/frontend/pages/root_page.dart';
 import 'package:project_fomo/style.dart';
 import 'package:provider/provider.dart';
-import 'package:project_fomo/backend/state_models/register_model.dart';
 import 'package:project_fomo/frontend/components/shared/gradient_button.dart';
 import 'package:project_fomo/frontend/components/shared/input_field.dart';
-import 'package:project_fomo/backend/services/auth_service.dart';
 
 class RegisterPage extends StatelessWidget {
   static const String pageRoute = '/register';
@@ -20,92 +17,84 @@ class RegisterPage extends StatelessWidget {
     String _name;
     String _username;
 
-    return StateWidget<RegisterModel>(
-      stateModel: RegisterModel(
-        authenticator: Provider.of<AuthService>(context),
-      ),
-      builder: (context, model, child) => Scaffold(
-        appBar: AppBar(
-          leading: new IconButton(
-            icon: new Icon(Icons.arrow_back_ios, color: Colors.white),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,
+    return Scaffold(
+      appBar: AppBar(
+        leading: new IconButton(
+          icon: new Icon(Icons.arrow_back_ios, color: Colors.white),
+          onPressed: () => Navigator.of(context).pop(),
         ),
-        body: model.isLoading
-            ? LoadingIndicator()
-            : Padding(
-                padding:
-                    EdgeInsets.only(bottom: 40, left: 20, right: 20, top: 30),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    PageHeader('Register'),
-                    SizedBox(
-                      height: 60,
-                    ),
-                    InputField(
-                      'Email Address',
-                      Icons.mail,
-                      (String input) {
-                        _email = input;
-                      },
-                    ),
-                    SizedBox(
-                      height: 30.0,
-                    ),
-                    InputField(
-                      'Name',
-                      Icons.person,
-                      (String input) {
-                        _name = input;
-                      },
-                    ),
-                    SizedBox(
-                      height: 30.0,
-                    ),
-                    InputField(
-                      'Username',
-                      Icons.done,
-                      (String input) {
-                        _username = input;
-                      },
-                    ),
-                    SizedBox(
-                      height: 30.0,
-                    ),
-                    InputField(
-                      'Password',
-                      Icons.lock,
-                      (String input) {
-                        _password = input;
-                      },
-                      true,
-                    ),
-                    SizedBox(
-                      height: 50.0,
-                    ),
-                    GradientButton(
-                      buttonText: Text(
-                        'Register',
-                        style: TextStyle(
-                          fontSize: AppFontSize.size22,
-                          fontFamily: AppFontFamily.family,
-                        ),
-                      ),
-                      buttonPressed: () async {
-                        bool success = await model.register(_email, _password);
-                        if (success) {
-                          Navigator.pushNamed(context, RootPage.pageRoute);
-                        }
-                      },
-                    ),
-                  ],
-                ),
-              ),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
       ),
+      body: false
+          ? LoadingIndicator()
+          : Padding(
+              padding:
+                  EdgeInsets.only(bottom: 40, left: 20, right: 20, top: 30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  PageHeader('Register'),
+                  SizedBox(
+                    height: 60,
+                  ),
+                  InputField(
+                    'Email Address',
+                    Icons.mail,
+                    (String input) {
+                      _email = input;
+                    },
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  InputField(
+                    'Name',
+                    Icons.person,
+                    (String input) {
+                      _name = input;
+                    },
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  InputField(
+                    'Username',
+                    Icons.done,
+                    (String input) {
+                      _username = input;
+                    },
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  InputField(
+                    'Password',
+                    Icons.lock,
+                    (String input) {
+                      _password = input;
+                    },
+                    true,
+                  ),
+                  SizedBox(
+                    height: 50.0,
+                  ),
+                  GradientButton(
+                    buttonText: Text(
+                      'Register',
+                      style: TextStyle(
+                        fontSize: AppFontSize.size22,
+                        fontFamily: AppFontFamily.family,
+                      ),
+                    ),
+                    buttonPressed: () {
+                      print('Register');
+                    },
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }

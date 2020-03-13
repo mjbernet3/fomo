23157c23d74e573d8ce3ae4bee26a6637f0cb8a1
Matthew Bernet <mattbernetGT@gmail.com>
2020-03-13 17:01:48 -0400

    Nothing honestly

--- lib/components/shared/gradient_icon.dart
+++ lib/components/shared/gradient_icon.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:project_fomo/style.dart';
 
-
-// Definitely needs some work on... doesn't really fit the gradient perfect
-// Perfect task for Jack our super detailed dude
-// Linear Gradient will NOT work... it'll only fill with one color
-// this method of a gradient icon needs a radial gradient
+// TODO: Make gradient look better
+// Linear Gradient will NOT work (so cannot use app gradient in stylesheet)
 
 class GradientIcon extends StatelessWidget {
-
   final IconData icon;
   final double size;
 
@@ -17,24 +12,23 @@ class GradientIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ShaderMask(
-        blendMode: BlendMode.srcIn,
-        shaderCallback: (Rect bounds) {
-          return RadialGradient(
-            focal: Alignment.centerLeft,
-            radius: 1.5,
-            colors: <Color>[
-              Color(0xFFFF6C1A),
-              Color(0xFFF01844),
-              Color(0xFF7E0BC9),
-            ],
-            tileMode: TileMode.mirror,
-
-          ).createShader(bounds);
-        },
-        child: Icon(
-            icon,
-        size: size,)
+      blendMode: BlendMode.srcIn,
+      shaderCallback: (Rect bounds) {
+        return RadialGradient(
+          focal: Alignment.centerLeft,
+          radius: 1.5,
+          colors: <Color>[
+            Color(0xFFFF6C1A),
+            Color(0xFFF01844),
+            Color(0xFF7E0BC9),
+          ],
+          tileMode: TileMode.mirror,
+        ).createShader(bounds);
+      },
+      child: Icon(
+        icon,
+        size: size,
+      ),
     );
   }
-
 }

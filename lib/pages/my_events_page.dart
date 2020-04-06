3064b487652c54c106cc6321919435c42da7c595
Aviva Kern <aviva.ashley@gmail.com>
2020-04-06 00:26:38 -0400

    Added a streamBuilder to My Events page to connect the event listing to firebase

--- lib/pages/my_events_page.dart
+++ lib/pages/my_events_page.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:project_fomo/components/shared/vertical_event_listing.dart';
 import 'package:project_fomo/components/shared/page_header.dart';
 import 'package:project_fomo/style.dart';
+import 'package:project_fomo/services/user_service.dart';
+import 'package:provider/provider.dart';
+import 'package:project_fomo/models/user_data.dart';
+import 'package:project_fomo/components/shared/loading_indicator.dart';
+import 'package:project_fomo/models/event.dart';
 
 class MyEventsPage extends StatelessWidget {
   static const String pageRoute = '/';
@@ -13,6 +18,9 @@ class MyEventsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final UserService _userService =
+      Provider.of<UserService>(context, listen: false);
+
     return DefaultTabController(
       length: 2,
       child: Scaffold(
@@ -33,8 +41,30 @@ class MyEventsPage extends StatelessWidget {
         body: TabBarView(
           children:
             [
-              VerticalEventListing(),
-              VerticalEventListing()
+              StreamBuilder(
+                stream: _userService.userData,
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return LoadingIndicator();
+                  }
+
+                  final UserData _userData = snapshot.data;
+
+                  return VerticalEventListing(events: List<Event>.from(_userData.going.where((i) => i.flag == true)));
+                }
+              ),
+              StreamBuilder(
+                  stream: _userService.userData,
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return LoadingIndicator();
+                    }
+
+                    final UserData _userData = snapshot.data;
+
+                    return VerticalEventListing(events: List<Event>.from(_userData.interested.where((i) => i.flag == true)));
+                  }
+              )
             ]
         ),
       ),

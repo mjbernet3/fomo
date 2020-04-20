43b6214fc7a453bfa5bd5ae6e9e32901b3e36571
Matthew Bernet <mattbernetGT@gmail.com>
2020-04-20 04:34:24 -0400

    Loading if discover does not have its data yet

--- lib/components/discover/discover_body.dart
+++ lib/components/discover/discover_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:project_fomo/blocs/discover_bloc.dart';
 import 'package:project_fomo/components/discover/event_scroller.dart';
 import 'package:project_fomo/components/discover/featured_carousel.dart';
+import 'package:project_fomo/components/shared/loading_indicator.dart';
 import 'package:project_fomo/components/shared/page_header.dart';
 import 'package:project_fomo/models/event.dart';
 import 'package:provider/provider.dart';
@@ -30,19 +31,21 @@ class _DiscoverBodyState extends State<DiscoverBody> {
         child: StreamBuilder(
           stream: _bloc.events,
           builder: (context, AsyncSnapshot<Map<String, List<Event>>> snapshot) {
+            if (!snapshot.hasData) {
+              return LoadingIndicator();
+            }
+
             List<Widget> listChildren = [];
-            if (snapshot.hasData) {
-              Map<String, List<Event>> categories = snapshot.data;
-              for (String category in categories.keys) {
-                List<Event> events = categories[category];
-                if (category == "featured" && events.length > 0) {
-                  listChildren.insert(0, FeaturedCarousel(events));
-                } else {
-                  listChildren.add(Padding(
-                    padding: const EdgeInsets.only(bottom: 15.0),
-                    child: EventScroller(formatCategoryTitle(category), events),
-                  ));
-                }
+            Map<String, List<Event>> categories = snapshot.data;
+            for (String category in categories.keys) {
+              List<Event> events = categories[category];
+              if (category == "featured" && events.length > 0) {
+                listChildren.insert(0, FeaturedCarousel(events));
+              } else {
+                listChildren.add(Padding(
+                  padding: const EdgeInsets.only(bottom: 15.0),
+                  child: EventScroller(formatCategoryTitle(category), events),
+                ));
               }
             }
             listChildren.insert(

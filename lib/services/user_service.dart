68009c9101d2605c4aa12877fb92913d473369c5
Matthew Bernet <mattbernetGT@gmail.com>
2020-04-20 03:24:53 -0400

    Make calls updates async

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -19,16 +19,6 @@ class UserService {
     });
   }
 
-  Response updateNotification(bool data) {
-    try {
-      _userDataCollection
-          .document(_userId)
-          .updateData({'shouldNotify': data});
-    } catch (error) {
-      return Response(status: Status.FAILURE, message: error.toString());
-    }
-  }
-
   Stream<UserData> otherUserData(String id) {
     return _userDataCollection
         .document(id)
@@ -49,11 +39,23 @@ class UserService {
     }
   }
 
-  Response updateLocation(bool data) {
+  Future<Response> updateLocation(bool data) async {
     try {
-      _userDataCollection
+      await _userDataCollection
           .document(_userId)
           .updateData({'shouldLocate': data});
+      return Response(status: Status.SUCCESS);
+    } catch (error) {
+      return Response(status: Status.FAILURE, message: error.toString());
+    }
+  }
+
+  Future<Response> updateNotification(bool data) async {
+    try {
+      await _userDataCollection
+          .document(_userId)
+          .updateData({'shouldNotify': data});
+      return Response(status: Status.SUCCESS);
     } catch (error) {
       return Response(status: Status.FAILURE, message: error.toString());
     }
@@ -68,56 +70,5 @@ class UserService {
     } catch (error) {
       return Response(status: Status.FAILURE, message: error.toString());
     }
-  }
-
-  Future<bool> addFriend(String userName) async {
-    // Assumes the userName property of all users is unique
-    UserData me = await this.userData.first;
-    print(me);
-    String myUserName = me.userName;
-    if (me.friends.contains(userName)) {
-      return false;
-      // throw Exception('user ${myUserName} is already friends with ${userName}');
-    }
-    UserData friend;
-    // Find target user
-    await _userDataCollection
-        .where('userName', isEqualTo: userName)
-        .limit(1)
-        .getDocuments()
-        .then((QuerySnapshot friends) {
-      if (friends.documents.length == 1) {
-        // Add my userName to target user's friend list
-        DocumentSnapshot doc = friends.documents[0];
-        friend = UserData.fromDocSnap(doc);
-        if (doc.data['friends'].contains(myUserName)) {
-          return false;
-//          throw Exception(
-//              'user ${userName} is already friends with ${myUserName}');
-        }
-        List<dynamic> newFriends = doc.data['friends'];
-        newFriends.add(myUserName);
-        _userDataCollection
-            .document(doc.documentID)
-            .updateData({'friends': newFriends});
-      } else {
-        return false;
-//        throw Exception(
-//            'user with userName ${userName} not found.'); // Logically equivalent to next possible Exception. Consider keeping only one.
-      }
-      return true;
-    });
-    if (friend == null) {
-      return false;
-//      throw Exception(
-//          'user with userName ${userName} not found.'); // Logically equivalent to previous possible Exception. Consider removing.
-    }
-    // Add target user to my friend list
-    List<dynamic> newFriends = me.friends;
-    newFriends.add(userName);
-    await _userDataCollection
-        .document(_userId)
-        .updateData({'friends': newFriends});
-    return true;
   }
 }

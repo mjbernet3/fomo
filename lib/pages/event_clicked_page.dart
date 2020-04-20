b1ec5c463fbd42cd958382bdf86a35d7e9af21fa
Matthew Bernet <mattbernetGT@gmail.com>
2020-04-20 05:10:51 -0400

    Launch urls on event clicked

--- lib/pages/event_clicked_page.dart
+++ lib/pages/event_clicked_page.dart
@@ -13,88 +13,82 @@ class EventClickedPage extends StatelessWidget {
     Event _event = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          leading: new IconButton(
-            icon: new Icon(
-              Icons.arrow_back_ios,
-              color: Colors.white,
-            ),
-            onPressed: () => Navigator.of(context).pop(),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        leading: new IconButton(
+          icon: new Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
           ),
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,
+          onPressed: () => Navigator.of(context).pop(),
         ),
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                EventHeader(imageURL: _event.imageUrl, eventName: _event.name),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Container(
-                      height: 2,
-                      width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(
-                        gradient: AppColor.gradient,
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(
-                            color: Color(0xFF343434),
-                          ),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              EventHeader(imageURL: _event.imageUrl, eventName: _event.name),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: Color(0xFF343434),
                         ),
                       ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          _event.description != null
-                              ? Padding(
-                                  padding: EdgeInsets.only(
-                                      left: 15, top: 25, right: 15, bottom: 15),
-                                  child: Text(
-                                    _event.description,
-                                    style: TextStyle(
-                                      fontFamily: AppFontFamily.family,
-                                      fontSize: AppFontSize.size18,
-                                      fontWeight: AppFontWeight.normal,
-                                    ),
-                                  ),
-                                )
-                              : SizedBox.shrink(),
-                          InfoLine(
-                            type: 'Location:',
-                            content: _event.venueName,
-                          ),
-                          InfoLine(
-                            type: 'Time:',
-                            content: _event.getDateTimeString(),
-                          ),
-                          InfoLine(
-                            type: 'Genre(s):',
-                            content: '${_event.genre}, ${_event.subGenre}',
-                          ),
-                        ],
-                      ),
                     ),
-                  ],
-                ),
-                OutputLink(
-                  title: 'Get Tickets',
-                  url: _event.ticketUrl,
-                  displayIcon: Icons.receipt,
-                ),
-                OutputLink(
-                  title: 'Get Directions',
-                  url: "formely the variable \"apple url\"",
-                  displayIcon: Icons.gps_fixed,
-                ),
-              ],
-            ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        _event.description != null
+                            ? Padding(
+                                padding: EdgeInsets.only(
+                                    left: 15, top: 25, right: 15, bottom: 15),
+                                child: Text(
+                                  _event.description,
+                                  style: TextStyle(
+                                    fontFamily: AppFontFamily.family,
+                                    fontSize: AppFontSize.size18,
+                                    fontWeight: AppFontWeight.normal,
+                                  ),
+                                ),
+                              )
+                            : SizedBox.shrink(),
+                        InfoLine(
+                          type: 'Location:',
+                          content: _event.venueName,
+                        ),
+                        InfoLine(
+                          type: 'Time:',
+                          content: _event.getDateTimeString(),
+                        ),
+                        InfoLine(
+                          type: 'Genre(s):',
+                          content: '${_event.genre}, ${_event.subGenre}',
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              OutputLink(
+                title: 'Get Tickets',
+                url: _event.ticketUrl,
+                displayIcon: Icons.receipt,
+              ),
+              OutputLink(
+                title: 'Get Directions',
+                url: "formely the variable \"apple url\"",
+                displayIcon: Icons.gps_fixed,
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
